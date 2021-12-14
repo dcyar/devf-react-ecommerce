a9d740b57f266c666e836fd9f4b3dc50ad1e3166
LeonidasNarc <narcisoleonidas97@gmail.com>
2021-12-13 20:43:28 -0500

    se termino login register y token

---
+++ src/views/Login/components/Formulario.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import useForm from "../../../hooks/useForm"
+import axios from 'axios'
+// import { useUserContext } from '../../../context/userContext'
+
+
+export default function Formulario() {
+
+    const history=useHistory();
+    // const context=useUserContext();
+
+    const login =(datos)=>{
+        axios
+        .post ("https://ecomerce-master.herokuapp.com/api/v1/login", datos)
+        .then(response=>{
+            window.localStorage.setItem('token', response.data.token)
+
+            // const config = {
+            //     headers: {
+            //       Authorization: `JWT ${response.data.token}`,
+            //     },
+            //   };
+            //   axios
+            //     .get("https://ecomerce-master.herokuapp.com/api/v1/user/me", config)
+            //     .then((response) => {
+            //       if (response.status === 200) {
+            //         context.setUsuarioActual(response.data);
+            //       }
+            //     });
+
+            history.push("/");
+        }).catch((error)=>{
+            console.log(error);
+
+            //aca se podria poner notificacion(hubo un errro)
+        })
+    }
+    const {inputs, handleInput,handleSubmit }=useForm(login,{})
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className='login' >
+            <h1>Acceso</h1>
+            <p>¿No tienes una cuenta?<Link to='/registrarse' > Crea uno</Link></p>
+        
+            <div className='login-contenedor'>
+            <div className='login-contenedor-input'>
+        
+                <label htmlFor='email'></label>
+                    <input
+                     type='text' 
+                     id='email' 
+                     name='email' 
+                     placeholder='correo electrónico*'
+                     onChange={handleInput}
+                     value={inputs.email}  
+                     />
+                </div>
+                <div className='login-contenedor-input'>
+                <label htmlFor='password'></label>
+                    <input
+                     type='password' 
+                     id='password' 
+                     name='password' 
+                     placeholder=' Tu contraseña*' 
+                     onChange={handleInput}
+                     value={inputs.password}  
+                     />
+                </div>
+                <div className='login-footer'>
+                
+                    <input className="check"
+                     type='checkbox'
+                     name='remember'
+                     id='remember' 
+                     
+                     />
+                     <label htmlFor='remember' >Recuérdame</label>
+                    <Link to='/'>¿Se te olvidó la contraseña?</Link>
+                </div>
+                <button className='btn' type='submit' >Acceso</button>
+
+
+            </div>
+           
+            </form>
+        </div>
+    )
+}
--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,72 +1,11 @@
 import React from "react";
+import FormularioRegister from "./components/FormularioRegister";
 import "./register.css";
 
 export default function Register() {
     return (
         <div className="register-wrap">
-            <form className="register">
-                <h1>Registrarse</h1>
-                <p>Por favor complete la siguiente informacion</p>
-
-                <div className="register-contenedor">
-                    <div className="register-contenedor-input">
-                        <label htmlFor="name"></label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            placeholder="Tu nombre"
-                        />
-                    </div>
-                    <div className="register-contenedor-input">
-                        <label htmlFor="correo"></label>
-                        <input
-                            type="text"
-                            id="correo"
-                            name="correo"
-                            placeholder="Tu correo electrónico*"
-                        />
-                    </div>
-                    <div className="register-contenedor-input">
-                        <label htmlFor="password"></label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Tu contraseña*"
-                        />
-                    </div>
-                    <div className="register-contenedor-input">
-                        <label htmlFor="password"></label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Confirmacion de contraseña*"
-                        />
-                    </div>
-                    <div>
-                        <p className="parrafo">Sus datos personales se utilizarán para respaldar su experiencia en este sitio web, para administrar el accseo a su cuenta y para otros fines descritos en nuestra política de privacidad </p>
-                    </div>
-                    <div className="register-footer">
-                        <input
-                            className="check"
-                            type="checkbox"
-                            name="remember"
-                            id="remember"
-                        />
-                        <label htmlFor="remember">Estoy de acuerdo con los términos y política</label>
-                        
-                    </div>
-                    <button className="btn" type="submit">
-                        Registrarse
-                    </button>
-                   <div>
-                       <spam className='cuenta'>¿Ya tienes una cuenta? <a href="#" >Acesso</a></spam>
-                   </div>
-    
-                </div>
-            </form>
+           <FormularioRegister/>
         </div>
     );
 }
