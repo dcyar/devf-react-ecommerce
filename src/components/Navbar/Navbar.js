a4a77390bc90c9a0417a1e44f90a4889c95cadff
Jhon Achata Limachi <jhonal183@gmail.com>
2021-12-14 22:09:59 -0500

    Mover los assets del navbar y crear el contexto del usuario

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,44 @@
 import React from 'react';
 import './Navbar.css';
-import {Link} from 'react-router-dom';
-import logo from '../../img/logo.svg';
-import cart from '../../img/cart.png';
-import account from '../../img/account.png';
-import search from '../../img/search.png';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/img/logo.svg';
+import cart from '../../assets/img/cart.png';
+import account from '../../assets/img/account.png';
+import search from '../../assets/img/search.png';
+import { useUserContext } from '../../context/userContext';
 
 
 export default function Navbar() {
-    const token = window.localStorage.getItem('token');
+    const { currentUser } = useUserContext();
     return (
         <div className='navbar'>
             <div className='nav_search'>
-                <img className='logo' src={logo} alt='Logo'/>
+                <img className='logo' src={logo} alt='Logo' />
                 <div className='searchbar'>
-                    <input type='text' name='search' placeholder='Search for items...'/>
-                    <img src={search} className='icon' alt='search'/>
+                    <input type='text' name='search' placeholder='Search for items...' />
+                    <img src={search} className='icon' alt='search' />
                 </div>
             </div>
             <div className='nav_options'>
-                {token ? 
+                {currentUser ?
                     <>
-                    <div className='nav_icon'>
-                        <img src={account} className='icon' alt='profile'></img>
-                        <Link to='/profile'>Bienvenido</Link>
-                    </div>
-                    <div className='nav_icon'>
-                        <img src={cart} className='icon' alt='cart'></img>
-                        <Link to='/carrito'>Carrito</Link>
-                    </div>
+                        <div className='nav_icon'>
+                            <img src={account} className='icon' alt='profile'></img>
+                            <Link to='/profile'>Bienvenido</Link>
+                        </div>
+                        <div className='nav_icon'>
+                            <img src={cart} className='icon' alt='cart'></img>
+                            <Link to='/carrito'>Carrito</Link>
+                        </div>
                     </>
                     :
                     <>
-                    <div className='nav_icon'>
-                        <Link to='/login'>Login</Link>
-                    </div>
-                    <div className='nav_icon'>
-                    <Link to='/registrarse'>Registro</Link>
-                    </div>
+                        <div className='nav_icon'>
+                            <Link to='/login'>Login</Link>
+                        </div>
+                        <div className='nav_icon'>
+                            <Link to='/registrarse'>Registro</Link>
+                        </div>
                     </>
                 }
             </div>
