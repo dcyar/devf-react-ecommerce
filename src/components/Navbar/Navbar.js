5984120c6731eb5f094a2257daa9316e4001bddc
YannYiss <adrian_nc@outlook.com>
2021-12-14 21:51:43 -0600

    CSS actualizado y en proceso

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,35 +8,37 @@ import search from '../../img/search.png';
 
 
 export default function Navbar() {
-    const token = window.localStorage.getItem('token');
     return (
         <div className='navbar'>
             <div className='nav_search'>
-                <img className='logo' src={logo} alt='Logo'/>
+                <Link to='/'><img className='logo' src={logo} alt='Logo'/></Link>
                 <div className='searchbar'>
                     <input type='text' name='search' placeholder='Search for items...'/>
                     <img src={search} className='icon' alt='search'/>
                 </div>
             </div>
             <div className='nav_options'>
-                {token ? 
+                {true ? 
                     <>
                     <div className='nav_icon'>
                         <img src={account} className='icon' alt='profile'></img>
-                        <Link to='/profile'>Bienvenido</Link>
+                        <Link to='/profile' className='nav_link'>Bienvenido</Link>
                     </div>
                     <div className='nav_icon'>
                         <img src={cart} className='icon' alt='cart'></img>
-                        <Link to='/carrito'>Carrito</Link>
+                        <Link to='/carrito' className='nav_link'>Carrito</Link>
+                    </div>
+                    <div className='nav_icon'>
+                        <Link to='/logout' className='nav_button' id='logout_button'>Logout</Link>
                     </div>
                     </>
                     :
                     <>
                     <div className='nav_icon'>
-                        <Link to='/login'>Login</Link>
+                        <Link to='/login' className='nav_button' id='login_button'>Login</Link>
                     </div>
                     <div className='nav_icon'>
-                    <Link to='/registrarse'>Registro</Link>
+                    <Link to='/registrarse' className='nav_button' id='signin_button'>Registro</Link>
                     </div>
                     </>
                 }
