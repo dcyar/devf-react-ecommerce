3b17ab2cb4e96a4f6881a239574f06c5c6153b6f
YannYiss <adrian_nc@outlook.com>
2021-12-15 21:14:44 -0600

    Navbar terminada con estilos y breakpoints responsivos

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './Navbar.css';
 import {Link} from 'react-router-dom';
 import logo from '../../img/logo.svg';
-import cart from '../../img/cart.png';
-import account from '../../img/account.png';
-import search from '../../img/search.png';
 
 
 export default function Navbar() {
+    const nav_options = useRef();
+    const toggle = () => {
+        if(nav_options.current.className === 'nav_options'){
+            nav_options.current.className = 'nav_options visible'
+        } else {
+            nav_options.current.className = 'nav_options'
+        }
+    }
     return (
         <div className='navbar'>
-            <div className='nav_search'>
-                <Link to='/'><img className='logo' src={logo} alt='Logo'/></Link>
-                <div className='searchbar'>
-                    <input type='text' name='search' placeholder='Search for items...'/>
-                    <img src={search} className='icon' alt='search'/>
-                </div>
+            <Link to='/'><img className='logo' src={logo} alt='Logo'/></Link>
+            <div className='searchbar'>
+                <input type='text' name='search' placeholder='Search for items...'/>
+                <i class="fas fa-search icon"></i>
             </div>
-            <div className='nav_options'>
+            <div className='nav_options' ref={nav_options}>
                 {true ? 
                     <>
+                    <div className='searchbar_hidden'>
+                        <input type='text' name='search' placeholder='Search for items...'/>
+                        <i class="fas fa-search icon"></i>
+                    </div>
                     <div className='nav_icon'>
-                        <img src={account} className='icon' alt='profile'></img>
+                        <i class="fas fa-user-alt icon" id="user_profile"></i>
                         <Link to='/profile' className='nav_link'>Bienvenido</Link>
                     </div>
                     <div className='nav_icon'>
-                        <img src={cart} className='icon' alt='cart'></img>
+                        <i class="fas fa-shopping-cart icon"></i>
                         <Link to='/carrito' className='nav_link'>Carrito</Link>
                     </div>
                     <div className='nav_icon'>
@@ -43,6 +50,7 @@ export default function Navbar() {
                     </>
                 }
             </div>
+            <button className='colapse_nav' onClick={toggle}><i class="fas fa-bars icon"></i></button>
         </div>
     )
 }
