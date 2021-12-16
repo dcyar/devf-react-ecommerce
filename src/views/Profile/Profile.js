3846a5494c06f36f77f35d8f54e9acdf0042b657
David Vera <dveracabanillas@gmail.com>
2021-12-16 18:27:39 -0500

    perfil básico

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useShopCartContext } from '../../context/cartContext';
+import { useUserContext } from '../../context/userContext';
 import withProtection from '../../utils/withProtection';
+import perfil from '../../assets/img/perfil.png'
+import './profile.css'
 
 const Profile = () => {
+    const { currentUser } = useUserContext();
     const { handleAddItemToShopCart, handleRemoveItemFromShopCart } = useShopCartContext()
 
     const handleAddItem = () => {
@@ -17,11 +21,34 @@ const Profile = () => {
         handleRemoveItemFromShopCart(31)
     }
     return (
-        <div>
-            <h1>Profile......</h1>
-            <button onClick={handleAddItem}>Add item</button>
-            <button onClick={handleRemoveItem}>Remove item</button>
-        </div>
+        <>
+            <div className='contenedorPerfil'>
+                <div className='contenedorPerfil__foto'>
+                    <h1>Perfil</h1>
+                    <img src={perfil} />
+                </div>
+                <div className='contenedorPerfil__contenido'>
+                    <div className='contenedorPerfil__nombre'>
+                        <h3>Nombre</h3>
+                        <p> {currentUser.first_name} {currentUser.last_name} </p>
+                    </div>
+                    <div className='contenedorPerfil__fechaNacimiento'>
+                        <h3>Fecha de nacimiento</h3>
+                        <p> {currentUser.birth_date} </p>
+                    </div>
+                    <div className='contenedorPerfil__genero'>
+                        <h3>Género</h3>
+                        <p> { currentUser.gender == 'M' ? <p>Masculino</p> : <p>Femenino</p>} </p>
+                    </div>
+                    <div className='contenedorPerfil__email'>
+                        <h3>Email</h3>
+                        <p> {currentUser.email} </p>
+                    </div>
+                </div>
+            </div>
+            {/*<button onClick={handleAddItem}>Add item</button>
+            <button onClick={handleRemoveItem}>Remove item</button>*/}
+        </>
     )
 }
 
