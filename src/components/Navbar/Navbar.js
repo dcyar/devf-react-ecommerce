a56f91343add2d25a28e526c465f14251ee26a5e
Jhon Achata Limachi <jhonal183@gmail.com>
2021-12-16 15:52:50 -0500

    corregir carrito de compras en el navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,19 @@
-import React, {useRef} from 'react';
-import './Navbar.css';
-import {Link} from 'react-router-dom';
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useUserContext } from '../../context/userContext';
+import { useShopCartContext } from '../../context/cartContext';
+import CartDropdown from './components/CartDropdown';
 import logo from '../../assets/img/logo.svg';
+import './Navbar.css';
 
 
 export default function Navbar() {
+    const { shopCart } = useShopCartContext();
+    const { currentUser } = useUserContext();
+    const [dropdownCart, setDropdownCart] = useState(true)
     const nav_options = useRef();
     const toggle = () => {
-        if(nav_options.current.className === 'nav_options'){
+        if (nav_options.current.className === 'nav_options') {
             nav_options.current.className = 'nav_options visible'
         } else {
             nav_options.current.className = 'nav_options'
@@ -15,38 +21,41 @@ export default function Navbar() {
     }
     return (
         <div className='navbar'>
-            <Link to='/'><img className='logo' src={logo} alt='Logo'/></Link>
+            <Link to='/'><img className='logo' src={logo} alt='Logo' /></Link>
             <div className='searchbar'>
-                <input type='text' name='search' placeholder='Search for items...'/>
+                <input type='text' name='search' placeholder='Search for items...' />
                 <i className="fas fa-search icon"></i>
             </div>
             <div className='nav_options' ref={nav_options}>
-                {true ? 
+                {currentUser ?
                     <>
-                    <div className='searchbar_hidden'>
-                        <input type='text' name='search' placeholder='Search for items...'/>
-                        <i className="fas fa-search icon"></i>
-                    </div>
-                    <div className='nav_icon'>
-                        <i className="fas fa-user-alt icon" id="user_profile"></i>
-                        <Link to='/profile' className='nav_link'>Bienvenido</Link>
-                    </div>
-                    <div className='nav_icon'>
-                        <i className="fas fa-shopping-cart icon"></i>
-                        <Link to='/carrito' className='nav_link'>Carrito</Link>
-                    </div>
-                    <div className='nav_icon'>
-                        <Link to='/logout' className='nav_button' id='logout_button'>Logout</Link>
-                    </div>
+                        <div className='searchbar_hidden'>
+                            <input type='text' name='search' placeholder='Search for items...' />
+                            <i className="fas fa-search icon"></i>
+                        </div>
+                        <div className='nav_icon'>
+                            <i className="fas fa-user-alt icon" id="user_profile"></i>
+                            <Link to='/profile' className='nav_link'>Bienvenido</Link>
+                        </div>
+                        <div className='nav_icon'>
+                            <i className="fas fa-shopping-cart icon"></i>
+                            <span onClick={() => setDropdownCart(!dropdownCart)} className='nav_link'>Carrito</span>
+                            {
+                                dropdownCart && <CartDropdown />
+                            }
+                        </div>
+                        <div className='nav_icon'>
+                            <Link to='/logout' className='nav_button' id='logout_button'>Logout</Link>
+                        </div>
                     </>
                     :
                     <>
-                    <div className='nav_icon'>
-                        <Link to='/login' className='nav_button' id='login_button'>Login</Link>
-                    </div>
-                    <div className='nav_icon'>
-                    <Link to='/registrarse' className='nav_button' id='signin_button'>Registro</Link>
-                    </div>
+                        <div className='nav_icon'>
+                            <Link to='/login' className='nav_button' id='login_button'>Login</Link>
+                        </div>
+                        <div className='nav_icon'>
+                            <Link to='/registrarse' className='nav_button' id='signin_button'>Registro</Link>
+                        </div>
                     </>
                 }
             </div>
