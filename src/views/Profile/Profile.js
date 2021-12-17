be9696c7400cbbfab4d198ff9cbbfaef5c5cbb39
Jhon Achata Limachi <jhonal183@gmail.com>
2021-12-16 19:11:28 -0500

    agregar boton para crear productos

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { useShopCartContext } from '../../context/cartContext';
 import { useUserContext } from '../../context/userContext';
 import withProtection from '../../utils/withProtection';
 import perfil from '../../assets/img/perfil.png'
 import './profile.css'
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
-    const { currentUser } = useUserContext();
-    const { handleAddItemToShopCart, handleRemoveItemFromShopCart } = useShopCartContext()
+    const { currentUser, role } = useUserContext();
 
-    const handleAddItem = () => {
-        handleAddItemToShopCart({
-            id: Math.floor(Math.random() * 100 + 1),
-            name: "Producto 1",
-            price: 123
-        });
-    }
-
-    const handleRemoveItem = () => {
-        handleRemoveItemFromShopCart(31)
-    }
     return (
         <>
             <div className='contenedorPerfil'>
                 <div className='contenedorPerfil__foto'>
                     <h1>Perfil</h1>
-                    <img src={perfil} />
+                    <img src={perfil} alt='Perfil de usuario' />
                 </div>
                 <div className='contenedorPerfil__contenido'>
                     <div className='contenedorPerfil__nombre'>
@@ -38,12 +26,20 @@ const Profile = () => {
                     </div>
                     <div className='contenedorPerfil__genero'>
                         <h3>Género</h3>
-                        <p> { currentUser.gender == 'M' ? <p>Masculino</p> : <p>Femenino</p>} </p>
+                        <p> {currentUser.gender === 'M' ? <p>Masculino</p> : <p>Femenino</p>} </p>
                     </div>
                     <div className='contenedorPerfil__email'>
                         <h3>Email</h3>
                         <p> {currentUser.email} </p>
                     </div>
+                    {
+                        currentUser.role === 'ADMIN' && <div className='contenedorPerfil__nombre'>
+                            <Link to="/crear-producto" >
+
+                                <button className='contenedorPerfil__boton'>Gestionar productos</button>
+                            </Link>
+                        </div>
+                    }
                 </div>
             </div>
             {/*<button onClick={handleAddItem}>Add item</button>
